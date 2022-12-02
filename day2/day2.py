421354b65cab3c0fb8da132e4970708839744b0e
William Johnson <wnj@tutamail.com>
2022-12-02 18:43:02 -0500

    Simplified

--- day2/day2.py
+++ day2/day2.py
@@ -1,7 +1,8 @@
 ROCK, PAPER, SCISSORS = 1, 2, 3
 OUTCOME_WIN, OUTCOME_LOSS, OUTCOME_DRAW = 6, 0, 3
-TO_WIN, TO_DRAW = 1, 0
+TO_WIN, TO_DRAW, TO_LOSE = 1, 0, -1
 
+OUTCOME_SCORE = {TO_WIN: OUTCOME_WIN, TO_LOSE: OUTCOME_LOSS, TO_DRAW: OUTCOME_DRAW}
 COMPUT_RPS = {'A': ROCK, 'B': PAPER, 'C': SCISSORS}
 PLAYER_RPS = {'X': ROCK, 'Y': PAPER, 'Z': SCISSORS}
 
@@ -10,14 +11,10 @@ player_total_score = 0
 f = open('data.txt', 'r')
 for line in f:
     comp_choice, player_choice = COMPUT_RPS[line[0]], PLAYER_RPS[line[2]]
-    round_result = (player_choice - comp_choice) % 3
+    
+    round_result = (player_choice - comp_choice + 1) % 3 - 1
 
-    if (round_result == TO_DRAW):
-        player_total_score += player_choice + OUTCOME_DRAW
-    elif (round_result == TO_WIN): 
-        player_total_score += player_choice + OUTCOME_WIN
-    else: 
-        player_total_score += player_choice + OUTCOME_LOSS
+    player_total_score += player_choice + OUTCOME_SCORE[round_result]
 
 f.close()
 
--- day2/day2_2.py
+++ day2/day2_2.py
@@ -2,6 +2,7 @@ ROCK, PAPER, SCISSORS = 1, 2, 3
 OUTCOME_WIN, OUTCOME_LOSS, OUTCOME_DRAW = 6, 0, 3
 TO_WIN, TO_DRAW, TO_LOSE = 1, 0, -1
 
+OUTCOME_SCORE = {TO_WIN: OUTCOME_WIN, TO_LOSE: OUTCOME_LOSS, TO_DRAW: OUTCOME_DRAW}
 COMPUT_RPS = {'A': ROCK, 'B': PAPER, 'C': SCISSORS}
 OUTCOME_RPS = {'X': TO_LOSE, 'Y': TO_DRAW, 'Z': TO_WIN}
 
@@ -11,15 +12,9 @@ f = open('data.txt', 'r')
 for line in f:
     comp_choice, player_outcome_choice = COMPUT_RPS[line[0]], OUTCOME_RPS[line[2]]
 
-    if (player_outcome_choice == TO_DRAW):
-        player_choice = (comp_choice-1) % 3 + 1
-        player_total_score += player_choice + OUTCOME_DRAW
-    elif (player_outcome_choice == TO_LOSE):
-        player_choice = (comp_choice-2) % 3 + 1
-        player_total_score += player_choice + OUTCOME_LOSS
-    else:
-        player_choice = (comp_choice) % 3 + 1
-        player_total_score += player_choice + OUTCOME_WIN
+    player_choice = (comp_choice + player_outcome_choice - 1) % 3 + 1
+
+    player_total_score += player_choice + OUTCOME_SCORE[player_outcome_choice]
 
 f.close()
 
