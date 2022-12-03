8e0a5d08b56ba6a0652597388d955a1988d2bdef
William Johnson <wnj@tutamail.com>
2022-12-02 22:17:26 -0500

    One-liner

--- day2/day2_min.py
+++ day2/day2_min.py
@@ -1,3 +1,1 @@
-ROCK, PAPER, SCISSORS, OUTCOME_WIN, OUTCOME_LOSS, OUTCOME_DRAW, TO_WIN, TO_DRAW, TO_LOSE = 1, 2, 3, 6, 0, 3, 1, 0, -1
-OUTCOME_SCORE, RPS = {TO_WIN: OUTCOME_WIN, TO_LOSE: OUTCOME_LOSS, TO_DRAW: OUTCOME_DRAW}, {'A': ROCK, 'B': PAPER, 'C': SCISSORS, 'X': ROCK, 'Y': PAPER, 'Z': SCISSORS}
-with open('data.txt', 'r') as f: print('Total player score = ' + str(sum([RPS[line[2]] + OUTCOME_SCORE[(RPS[line[2]] - RPS[line[0]] + 1) % 3 - 1] for line in f])) + '.')
+with open('data.txt', 'r') as f: OUTCOME_SCORE, RPS = {1: 6, -1: 0, 0: 3}, {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3} ; print('Total player score = ' + str(sum([RPS[line[2]] + OUTCOME_SCORE[(RPS[line[2]] - RPS[line[0]] + 1) % 3 - 1] for line in f])) + '.')
