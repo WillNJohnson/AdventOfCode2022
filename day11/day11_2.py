78e332fe0d617cd8a6a4548c664f8f84fcd77368
William Johnson <wnj@tutamail.com>
2022-12-11 14:36:45 -0500

    Part Two

--- day11/day11_2.py
+++ day11/day11_2.py
@@ -5,17 +5,18 @@ def prod(l):
     return [(x:=x*y) for y in l][-1]
 
 def doNProcessThrows(N, monkey):
+    common_modulus = prod([monkey[n]['divisible'] for n in range(len(monkey))])
     for _ in range(N):
-        processThrow(monkey)
+        processThrow(monkey, common_modulus)
     return monkey
 
-def processThrow(monkey):
+def processThrow(monkey, common_modulus):
     new = 0
     for n in range(len(monkey)):
         starting_items = monkey[n]['starting']
         while starting_items != []:
             old = monkey[n]['starting'][0]
-            new = eval(monkey[n]['op']) // 3
+            new = eval(monkey[n]['op']) % common_modulus
             if new % monkey[n]['divisible'] == 0:
                 monkey[monkey[n]['on_true']]['starting'].append(new)
             else:
@@ -36,7 +37,7 @@ with open('data.txt', 'r') as f:
         data = line.split()
 
         if data == []:
-            monkey[n] = {'starting': starting_items, 'op': operation, 'divisible': test_divisibility_by, 'on_true': on_true_throw_to, 'on_false': on_false_throw_to, 'inspections': 0}
+            monkey[n] = {'starting': starting_items, 'op': operation, 'divisible': test_divisibility_by, 'on_true': on_true_throw_to, 'on_false': on_false_throw_to, 'inspections': 0, 'state': 0}
             n += 1
         elif data[0] == 'Starting':
             starting_items = [int(d.strip(',')) for d in data[2:]]
@@ -49,12 +50,12 @@ with open('data.txt', 'r') as f:
         elif ' '.join(data[0:2]) == 'If false:':
             on_false_throw_to = int(data[-1])
 
-    monkey[n] = {'starting': starting_items, 'op': operation, 'divisible': test_divisibility_by, 'on_true': on_true_throw_to, 'on_false': on_false_throw_to, 'inspections': 0}
+    monkey[n] = {'starting': starting_items, 'op': operation, 'divisible': test_divisibility_by, 'on_true': on_true_throw_to, 'on_false': on_false_throw_to, 'inspections': 0, 'state': 0}
 
 # do calculation of monkey throws
-monkey = doNProcessThrows(20, monkey)
+monkey = doNProcessThrows(10000, monkey)
 
 # get top 2 number of inspections
 top_two_inspections = sorted([monkey[n]['inspections'] for n in monkey])[-2:]
 
-print(f'The level of monkey business after 20 rounds is {prod(top_two_inspections)}.')
+print(f'The level of monkey business after 10000 rounds is {prod(top_two_inspections)}.')
